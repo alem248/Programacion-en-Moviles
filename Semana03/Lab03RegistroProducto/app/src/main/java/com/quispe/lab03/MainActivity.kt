8f501f85e2e568ce70b726b12804aba09aa2b10e
Diego Nina <diego.nina.m@tecsup.edu.pe>
2026-09-02 14:51:31 -0500

    agrega boton y card de resumen con importe calculado

--- Semana03/Lab03RegistroProducto/app/src/main/java/com/quispe/lab03/MainActivity.kt
+++ Semana03/Lab03RegistroProducto/app/src/main/java/com/quispe/lab03/MainActivity.kt
@@ -12,6 +12,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -23,6 +26,8 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.quispe.lab03.ui.theme.Lab03Theme
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,8 +36,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Lab03Theme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    PantallaRegistro(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -97,7 +101,37 @@ fun PantallaRegistro(modifier: Modifier = Modifier) {
                 modifier = Modifier.weight(1f)
             )
         }
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(
+            onClick = { mostrarResumen = true },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("AGREGAR PRODUCTO")
+        }
+
+        Spacer(modifier = Modifier.height(24.dp))
+        if (mostrarResumen) {
+            val precioNum = precio.toDoubleOrNull() ?: 0.0
+            val cantidadNum = cantidad.toIntOrNull() ?: 0
+            val importe = 0.0 //
+            val total = precioNum * cantidadNum
+
+            Card(
+                modifier = Modifier.fillMaxWidth(),
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                )
+            ) {
+                Column(modifier = Modifier.padding(16.dp)) {
+                    Text(nombre, style = MaterialTheme.typography.titleLarge)
+                    Text("Precio: S/ " + String.format("%.2f", precioNum))
+                    Text("Cantidad: " + cantidad)
+                    Text("Importe total: S/ " + total)
+                }
+            }
+        }
     }
+
 }
 
 
