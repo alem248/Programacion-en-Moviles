4551229f6d355fc48d43036da15003ef02ffa072
alem248 <https://github.com/alem248>
2026-09-23 00:52:01 -0500

    feat(main): Agregar AppPreview en MainActivity

--- Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/MainActivity.kt
+++ Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/MainActivity.kt
@@ -4,7 +4,11 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import com.quispe.appnavegation.navigation.AppNavigation
+import com.quispe.appnavegation.screens.HomeScreen
 import com.quispe.appnavegation.ui.theme.AppNavegationTheme
 
 class MainActivity : ComponentActivity() {
@@ -16,5 +20,13 @@ class MainActivity : ComponentActivity() {
                 AppNavigation()
             }
         }
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun AppPreview() {
+    AppNavegationTheme {
+        HomeScreen(navController = rememberNavController())
     }
 }
