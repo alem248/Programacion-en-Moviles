bb6a6bb98530f1b4143b1f7cf39b88c594c46988
alem248 <https://github.com/alem248>
2026-09-23 00:22:58 -0500

    feat(navigation): Configurar rutas de navegación e integrar la pantalla de Login

--- Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/navigation/AppNavigation.kt
+++ Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/navigation/AppNavigation.kt
@@ -6,8 +6,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import com.quispe.appnavegation.screens.*
-import com.quispe.appnavegation.screens.compose.foundation.lazy.LazyColumn.ListScreen
+import com.quispe.appnavegation.screens.DetailScreen
+import com.quispe.appnavegation.screens.HomeScreen
+import com.quispe.appnavegation.screens.ListScreen
+import com.quispe.appnavegation.screens.LoginScreen
+import com.quispe.appnavegation.screens.ProfileScreen
 
 @Composable
 fun AppNavigation() {
@@ -15,8 +18,11 @@ fun AppNavigation() {
 
     NavHost(
         navController = navController,
-        startDestination = Screen.Home.route
+        startDestination = Screen.Login.route
     ) {
+        composable(Screen.Login.route) {
+            LoginScreen(navController)
+        }
         composable(Screen.Home.route) {
             HomeScreen(navController)
         }
@@ -31,11 +37,11 @@ fun AppNavigation() {
             arguments = listOf(
                 navArgument(name = "itemId") {
                     type = NavType.IntType
-                    defaultValue = 0
+                    defaultValue = 1
                 }
             )
         ) { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 0
+            val itemId = backStackEntry.arguments?.getInt("itemId") ?: 1
             DetailScreen(navController, itemId)
         }
     }
--- Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/navigation/Screen.kt
+++ Semana05/Navegation/app/src/main/java/com/quispe/appnavegation/navigation/Screen.kt
@@ -1,6 +1,7 @@
 package com.quispe.appnavegation.navigation
 
 sealed class Screen(val route: String) {
+    object Login : Screen(route = "login")
     object Home : Screen(route = "home")
     object List : Screen(route = "list")
     object Profile : Screen(route = "profile")
